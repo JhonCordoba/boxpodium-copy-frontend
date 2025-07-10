81fe5d483cdbc64c6c0039d2e013f59f7069f704
Gabriela Muñoz <nupan1024@gmail.com>
2025-07-10 17:45:37 -0500

    fix: adjust padding, hiden image when is device screens, darkers borders and text of inputs

--- src/views/sign-up/sign-up.tsx
+++ src/views/sign-up/sign-up.tsx
@@ -4,7 +4,7 @@ import { SignUpForm } from "../../components/sign-up-form";
 export function SignUp() { 
     return (
         <div className="w-full min-h-screen flex flex-col md:flex-row">
-            <div className="w-full h-full md:h-auto flex bg-[#F1F2F6]">
+            <div className="hidden md:flex w-full h-full md:h-auto bg-[#F1F2F6]">
                 <Banner />
             </div>
             <div className="w-full h-full  md:h-auto flex">
